cc0e7edb220777a60636111a1e1ef38d8ed96618
Kaloyan Veselinov <kvesselinov@gmail.com>
2017-09-17 16:38:02 +0200

    Front-end V1
    Premier jet du front-end, structure du code, librairies...

---
+++ public/src/php/form.php
@@ -0,0 +1,28 @@
+<center>
+
+  <div class="vertical-align-section img-thumbnail" id="passwordForm">
+
+    <form action="index.html" method="post">
+
+      <div class="form-group">
+        <label>Please enter this password 5 times:</label>
+        <pre>LoremIpsumPass</pre>
+      </div>
+
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" name="username" class="form-control">
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" name="password" class="form-control">
+      </div>
+
+      <input type="submit" value="Submit" class="btn btn-block">
+
+    </form>
+
+  </div>
+
+</center>
---
+++ public/src/php/include.php
@@ -0,0 +1,13 @@
+<!-- jQuery -->
+<script type="text/javascript" src="lib/js/jquery-3.2.1.min.js"></script>
+
+<!-- bootstrap -->
+<script type="text/javascript" src="lib/js/bootstrap.js" ></script>
+<link href="lib/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+
+<!-- font awesome -->
+<link href="lib/css/font-awesome.css" rel="stylesheet" type="text/css" media="all">
+
+<script type="text/javascript" scr="src/js/main.js"></script>
+
+<link href="src/css/main.css" rel="stylesheet" type="text/css" media="all">
