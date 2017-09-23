72e009d12963f954399d3c98db50c3ce866527ed
Jules Berry <jules.berry@protonmail.com>
2017-09-23 22:14:39 +0200

    adding log file

--- public/src/php/dataReceiver.php
+++ public/src/php/dataReceiver.php
@@ -1,5 +1,6 @@
 <?php
     include "databaseConnector.php";
+    include "logger.php";
     if(isset($_POST['data'])) {
         $entryData = $_POST['data'];
         foreach ($entryData as $entry) {
@@ -9,11 +10,11 @@
 
   function processEntry($entry,$connection){
     $entryId = addEntryToDatabase($entry,$connection);
-    echo $entryId;
-    processKeyEvents($entryId, $entry['keyUpEvents']);
-    processKeyEvents($entryId, $entry['keyDownEvents']);
+    logEntry($entryId);
+    //processKeyEvents($entryId, $entry['keyUpEvents']);
+    //processKeyEvents($entryId, $entry['keyDownEvents']);
   }
-
+/*
   function processKeyUpEvents($entryId, $keyEvents){
     processKeyEvents($entryId, "keyup", $keyEvents);
   }
@@ -30,3 +31,4 @@
       echo "$property => $value \n";
     }
   }
+*/
